3f54d81b6cbc8c2b0ff1f0b4f7b5910bafd7389d
John Kenneth Mariano <kenneth.mariano@paymaya.com>
2021-05-01 19:19:32 +0800

    Added dependencies and initial version of stacks

--- bin/cdk.js
+++ bin/cdk.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-const cdk = require('@aws-cdk/core');
-const { CdkStack } = require('../lib/cdk-stack');
+const CDK = require('@aws-cdk/core');
+const { S3WebsiteStack } = require('../lib/S3WebsiteStack');
+const { ServerlessAPIStack } = require('../lib/ServerlessAPIStack');
 
-const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+const app = new CDK.App();
+new S3WebsiteStack(app, 'S3WebsiteStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +19,9 @@ new CdkStack(app, 'CdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+
+  appName: "example",
+  domainName: "example.com"
 });
+
+new ServerlessAPIStack(app, 'ServerlessAPIStack', {});
